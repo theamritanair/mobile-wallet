d7b3a4269dadded06d2e8fd8b6bad7a6671ed79f
Amrita <theamritanair@gmail.com>
2019-01-29 18:38:05 +0530

    Passcode Documentation

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/passcode/presenter/PassCodePresenter.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/passcode/presenter/PassCodePresenter.java
@@ -11,7 +11,9 @@ import org.mifos.mobilewallet.mifospay.passcode.PassCodeContract;
 import javax.inject.Inject;
 
 /**
- * Created by ankur on 15/May/2018
+ * This class contains the components of the Presenter required for the PassCode
+ * @author ankur
+ * @since 15/May/2018
  */
 public class PassCodePresenter implements PassCodeContract.PassCodePresenter {
     private final UseCaseHandler mUsecaseHandler;
@@ -22,18 +24,26 @@ public class PassCodePresenter implements PassCodeContract.PassCodePresenter {
     FetchClientData fetchClientDataUseCase;
     private PassCodeContract.PassCodeView mPassCodeView;
 
+
     @Inject
     public PassCodePresenter(UseCaseHandler useCaseHandler, PreferencesHelper preferencesHelper) {
         this.mUsecaseHandler = useCaseHandler;
         this.preferencesHelper = preferencesHelper;
     }
 
+    /**
+     * This function attaches a view.
+     * @param baseView The view which is set as the PassCodeView
+     */
     @Override
     public void attachView(BaseView baseView) {
         mPassCodeView = (PassCodeContract.PassCodeView) baseView;
         mPassCodeView.setPresenter(this);
     }
 
+    /**
+     * This functions creates an authenticated service.
+     */
     public void createAuthenticatedService() {
         FineractApiManager.createSelfService(preferencesHelper.getToken());
     }
